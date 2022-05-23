b5f4830499bd1cb1e327a3d0cf51edd5a42ee2c3
Dave Hart <daveihart@hotmail.com>
2022-05-23 09:51:30 +0100

    Update mirror.sh
    Explicit push only to HEAD

--- mirror.sh
+++ mirror.sh
@@ -5,4 +5,4 @@ set -eu
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
-git push --tags mirror
+git push --tags mirror HEAD
