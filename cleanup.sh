353ab7df5a5a2ded1b780ac6a63ce6fd1275a260
Dinesh Addala <addalad@amazon.com>
2025-06-18 11:20:58 +0100

    Disabled debug and removed debug lines

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-# Check and remove HEAD if it exists
-if git ls-remote "$1" HEAD >/dev/null 2>&1; then
-  echo "Removing HEAD reference from mirror repository"
-  git push "$1" :HEAD
+# Check and remove refs/heads/HEAD if it exists
+if git ls-remote "$1" refs/heads/HEAD >/dev/null 2>&1; then
+  echo "Removing refs/heads/HEAD reference from mirror repository"
+  git push "$1" :refs/heads/HEAD
 else
   echo "No HEAD reference found in mirror repository"
 fi
